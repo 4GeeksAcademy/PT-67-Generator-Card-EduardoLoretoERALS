452a6864c289a8b6c908b59bbe6fabd92ca2e691
EduardoLoreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-05-23 19:57:50 +0000

    Modificacion del codigo JS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSimbol());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  document.querySelector(".btn").addEventListener("click", window.onload);
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomNumber = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+};
+
+let generateRandomSimbol = () => {
+  let simbol = ["♦", "♥", "♠", "♣"];
+  let indexsimbol = Math.floor(Math.random() * simbol.length);
+  return simbol[indexsimbol];
 };
